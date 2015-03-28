c107e47c033493a7b553a846593154cf1fa38aa7
Bastiaan Quast <bquast@gmail.com>
2015-03-28 11:04:18 +0100

    merge wwz data, ignore merged file (size > 50 mb github limit)

---
+++ R/merge-wwz.R
@@ -0,0 +1,18 @@
+# merge-wwz.R
+# Bastiaan Quast
+# bquast@gmail.com
+
+# load the data
+load("data/w1995.RData")
+load("data/w2000.RData")
+load("data/w2005.RData")
+load("data/w2008.RData")
+
+# add year variable to each data set
+w1995 <- cbind(year = 1995, w1995)
+w2000 <- cbind(year = 2000, w2000)
+w2005 <- cbind(year = 2005, w2005)
+w2008 <- cbind(year = 2008, w2008)
+
+# stack the data sets
+w1995-2008 <- rbind(w1995, w2000, w2005, w2008)
