3a8b4512a5458267e2cf849584e1b442825ad2af
Bastiaan Quast <bquast@gmail.com>
2015-03-30 15:06:19 +0200

    fixes to underscore

--- R/merge-wwz.R
+++ R/merge-wwz.R
@@ -15,4 +15,13 @@ w2005 <- cbind(year = 2005, w2005)
 w2008 <- cbind(year = 2008, w2008)
 
 # stack the data sets
-w1995-2008 <- rbind(w1995, w2000, w2005, w2008)
+w1995_2008 <- rbind(w1995, w2000, w2005, w2008)
+
+
+# to lower case
+w1995_2008["Exporting_Country"]  <- tolower( w1995_2008["Exporting_Country"] )
+w1995_2008["Exporting_Indsutry"] <- tolower( w1995_2008["Exporting_Industry"] )
+w1995_2008["Importing_Country"]  <- tolower( w1995_2008["Importing_Country"] )
+
+# save the data
+save(w1995_2008, file = "data/w1995_2008.RData")
