4e9b79e392ba53daf43bf86a29c7b5e5c6ebeb97
Bastiaan Quast <bquast@gmail.com>
2015-04-13 07:37:47 +0200

    merge from win and lin comps

--- R/merge-wwz.R
+++ R/merge-wwz.R
@@ -17,8 +17,6 @@ w2008 <- cbind(year = 2008, w2008)
 # stack the data sets
 w1995_2008 <- rbind(w1995, w2000, w2005, w2008)
 
-<<<<<<< HEAD
-
 # to lower case
 w1995_2008["Exporting_Country"]  <- tolower( w1995_2008["Exporting_Country"] )
 w1995_2008["Exporting_Indsutry"] <- tolower( w1995_2008["Exporting_Industry"] )
@@ -26,7 +24,3 @@ w1995_2008["Importing_Country"]  <- tolower( w1995_2008["Importing_Country"] )
 
 # save the data
 save(w1995_2008, file = "data/w1995_2008.RData")
-=======
-# save the data
-save(w1995_2008, file = "data/w1995_2008.RData" )
->>>>>>> 08d2e4f969dd117f45a58f120e274cad80214d03
