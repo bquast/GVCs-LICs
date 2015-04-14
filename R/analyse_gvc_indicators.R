198250c31a3b8b928a60a7d58f73ce0934859135
Bastiaan Quast <bquast@gmail.com>
2015-04-14 15:50:08 +0200

    double line plots

--- R/analyse_gvc_indicators.R
+++ R/analyse_gvc_indicators.R
@@ -33,13 +33,11 @@ gvc_indicators %>%
 # both
 gvc_indicators %>%
   group_by(year) %>%
-  summarise(e2r = sum(dvar_ik) / sum(exports_ik), i2e = sum(fvax_ik) / sum(exports_ik) ) %>%
+  summarise(e2r = sum(dvar_ik) / sum(exports_ik),
+            i2e = sum(fvax_ik) / sum(exports_ik) ) %>%
   ggvis(~year, ~e2r ) %>%
   layer_lines(stroke="e2r") %>%
   layer_lines(~year, ~i2e, stroke="i2e")
-
-# add PVC
-# add intermediate imports (?)
 
 # plot nrca
 nrca %>%
@@ -47,15 +45,29 @@ nrca %>%
   ggvis(~year, ~nrca) %>%
   layer_lines()
 
+# add PVC
+# add intermediate imports (?)
+
 # plot wwz
+# RDV
 w1995_2008 %>%
   group_by(year) %>%
   summarise( RDV = sum(RDV_INT) + sum(RDV_FIN) + sum(RDV_FIN2) ) %>%
   ggvis(~year, ~RDV) %>%
   layer_lines()
 
+# PDC
 w1995_2008 %>%
   group_by(year) %>%
   summarise( PDC = sum(DDC_FIN) + sum(DDC_INT) + sum(ODC) + sum(MDC) ) %>%
   ggvis(~year, ~PDC) %>%
   layer_lines()
+
+# both
+w1995_2008 %>%
+  group_by(year) %>%
+  summarise( RDV = sum(RDV_INT) + sum(RDV_FIN) + sum(RDV_FIN2),
+             PDC = sum(DDC_FIN) + sum(DDC_INT) + sum(ODC) + sum(MDC) ) %>%
+  ggvis(~year, ~RDV, stroke="RDV") %>%
+  layer_lines() %>%
+  layer_lines(~year, ~PDC, stroke="PDC")
