52634095231f04372e21b79bae04afdf17f6a801
Bastiaan Quast <bquast@gmail.com>
2016-01-27 12:12:00 +0100

    use new names for data and add new years

--- R/decompose.R
+++ R/decompose.R
@@ -6,29 +6,47 @@
 library(decompr)
 
 # run the Leontief decomposition
-l1995 <- decomp(wid1995,
-                wfd1995,
+l1995 <- decomp(inter1995,
+                final1995,
                 countries,
-                sectors,
+                industries,
                 output1995,
                 method = "leontief")
-l2000 <- decomp(wid2000,
-                wfd2000,
+l2000 <- decomp(inter2000,
+                final2000,
                 countries,
-                sectors,
+                industries,
                 output2000,
                 method = "leontief")
-l2005 <- decomp(wid2005,
-                wfd2005,
+l2005 <- decomp(inter2005,
+                final2005,
                 countries,
-                sectors,
+                industries,
                 output2005,
                 method = "leontief")
-l2008 <- decomp(wid2008,
-                wfd2008,
+l2008 <- decomp(inter2008,
+                final2008,
                 countries,
-                sectors,
+                industries,
                 output2008,
+                method = "leontief")
+l2009 <- decomp(inter2009,
+                final2009,
+                countries,
+                industries,
+                output2009,
+                method = "leontief")
+l2010 <- decomp(inter2010,
+                final2010,
+                countries,
+                industries,
+                output2010,
+                method = "leontief")
+l2011 <- decomp(inter2011,
+                final2011,
+                countries,
+                industries,
+                output2011,
                 method = "leontief")
 
 # save Leontief the data
@@ -36,29 +54,29 @@ save(l1995, l2000, l2005, l2008, file = "data/TiVa-Leontief.RData")
 
 
 
-# run the Wang-Wei-Zhu decomposition 
-w1995 <- decomp(wid1995,
-                wfd1995,
+# run the Wang-Wei-Zhu decomposition
+w1995 <- decomp(inter1995,
+                final1995,
                 countries,
-                sectors,
+                industries,
                 output1995,
                 method = "wwz")
-w2000 <- decomp(wid2000,
-                wfd2000,
+w2000 <- decomp(inter2000,
+                final2000,
                 countries,
-                sectors,
+                industries,
                 output2000,
                 method = "wwz")
-w2005 <- decomp(wid2005,
-                wfd2005,
+w2005 <- decomp(inter2005,
+                final2005,
                 countries,
-                sectors,
+                industries,
                 output2005,
                 method = "wwz")
-w2008 <- decomp(wid2008,
-                wfd2008,
+w2008 <- decomp(inter2008,
+                final2008,
                 countries,
-                sectors,
+                industries,
                 output2008,
                 method = "wwz")
 
