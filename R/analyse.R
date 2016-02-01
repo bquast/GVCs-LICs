b2910d4cb37182618bdc73270a6e5c3144c45ecd
Bastiaan Quast <bquast@gmail.com>
2016-02-01 14:39:25 +0100

    and NRCA for new data here

--- R/analyse.R
+++ R/analyse.R
@@ -12,3 +12,21 @@ rca["abs_rca_diff"] <- abs(rca["rca_difference"])
 ordered_rca <- rca[ order(rca["abs_rca_diff"]), ]
 
 save(rca, ordered_rca, file = "data/rca.RData")
+
+
+# load Leontief
+load(file = 'data/TiVa-Leontief.RData')
+
+
+# NRCA decomposition
+nrca1995 <- nrca(l1995)
+nrca2000 <- nrca(l2000)
+nrca2005 <- nrca(l2005)
+nrca2008 <- nrca(l2008)
+nrca2009 <- nrca(l2009)
+nrca2010 <- nrca(l2010)
+nrca2011 <- nrca(l2011)
+
+
+# save NRCAs
+save(nrca1995, nrca2000, nrca2005, nrca2008, nrca2009, nrca2010, nrca2011, file ="data/nrca.RData")
