4406ed914b12a71abc9694575e1e24504b661680
Bastiaan Quast <bquast@gmail.com>
2016-03-02 16:21:55 +0100

    country_chars imports

---
+++ R/country_chars.R
@@ -0,0 +1,11 @@
+# redo 2016
+# Bastiaan Quast
+# bquast@gmail.com
+
+
+# import data
+country_chars <- read.csv(file = 'data/country_chars.csv')
+
+
+# save data
+save(country_chars, file = 'data/country_chars.RData')
