96bf15c8641b4c2c8471e4108122df43ec9d77e3
Bastiaan Quast <bquast@gmail.com>
2016-05-19 09:41:42 +0200

    add plots

--- R/last_minute_stuff.R
+++ R/last_minute_stuff.R
@@ -33,6 +33,10 @@ sector_desc %>%
 wwz1 <- wwz[2:8,1:3]
 names(wwz1) <- c('year', 'e2rL-LM', 'i2eL-LM')
 wwz1$year <- as.numeric(wwz1$year)
+wwz1$`e2rL-LM` <- as.numeric(wwz1$`e2rL-LM`)
+wwz1$`i2eL-LM` <- as.numeric(wwz1$`i2eL-LM`)
+wwz1$`e2rL-LM` <- round(wwz1$`e2rL-LM`)
+
 wwz2 <- wwz[11:17,]
 names(wwz2) <- wwz[10,]
 names(wwz2)[1] <- 'year'
@@ -47,3 +51,10 @@ wwz1 %>%
   ggvis(x=~year, y=~`e2rL-LM`, stroke='e2r') %>%
   layer_lines() %>%
   layer_lines(x=~year, y=~`i2eL-LM`, stroke='i2e')
+
+wwz2 %>%
+  ggvis(x=~year, y=~fva_fin, stroke='fva_fin') %>%
+  layer_lines() %>%
+  layer_lines(x=~year, y=~fva_inter, stroke='fva_inter') %>%
+  layer_lines(x=~year, y=~dva_fin,   stroke='dva_fin')   %>%
+  layer_lines(x=~year, y=~dva_inter, stroke='dva_inter')
